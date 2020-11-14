188d699d292953ede90d7d421489fd2b17b6db95
R-Jin <ryanji007@gmail.com>
2020-11-14 14:52:54 +0100

    animated the hamburger menu

--- src/scripts/ham_toggle.js
+++ src/scripts/ham_toggle.js
@@ -1,15 +1,23 @@
-var menu = document.getElementById("menu");
-var nav = document.getElementById("nav");
-var exit = document.getElementById("exit");
+// var menu = document.getElementById("menu");
+// var nav = document.getElementById("nav");
+// var exit = document.getElementById("exit");
 
-menu.addEventListener("click", (e) => {
+// menu.addEventListener("click", (e) => {
+//     nav.classList.toggle("hide-mobile");
+//     menu.style.display = "none";
+//     e.preventDefault();
+// }) 
+
+// exit.addEventListener("click", (e) => {
+//     nav.classList.add("hide-mobile");
+//     menu.style.display = "block";
+//     e.preventDefault();
+// }) 
+
+var hamburger = document.querySelector(".hamburger");
+  // On click
+  hamburger.addEventListener("click", function() {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
     nav.classList.toggle("hide-mobile");
-    menu.style.display = "none";
-    e.preventDefault();
-}) 
-
-exit.addEventListener("click", (e) => {
-    nav.classList.add("hide-mobile");
-    menu.style.display = "block";
-    e.preventDefault();
-}) 
+})
