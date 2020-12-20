67326223a1f84202c97f31ccf69b2d4da4b2ac30
R-Jin <ryanji007@gmail.com>
2020-12-20 20:28:10 +0100

    fix fig1

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -2755,7 +2755,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "22907" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "23180" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
